f92bd8cc2c8544ae7a3109863a237fa2a61ce879
seantsao00 <seantsao00@gmail.com>
2024-12-09 12:15:55 +0800

    feat(cors): enable CORS for local development

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+import cors from 'cors';
 import express from 'express';
 import router from './routes';
 
 const app = express();
 const port = 3030;
+
+app.use(cors({
+  origin: 'http://localhost:*',
+}));
 
 app.use(router);
 
