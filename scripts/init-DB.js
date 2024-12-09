c807be862d30d29a43ecfc364e19c9293b8e5538
Yao <changmingyao2014@gmail.com>
2024-12-09 21:29:13 +0800

    fix(init): bug fix on final decision

--- scripts/init-DB.js
+++ scripts/init-DB.js
@@ -9,91 +9,92 @@ const initDb = async () => {
   console.log('Starting database initialization...');
   try {
     // will fail if exists, so executeQuery cannot be used
-    await pool.query(`
-      CREATE DATABASE ${process.env.PUGDATABASE};
-    `);
+    // await pool.query(`
+    //   CREATE DATABASE ${process.env.PGDATABASE};
+    // `);
 
     await executeQuery(pool, `
-      CREATE TABLE IF NOT EXISTS temp_user (
-    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name varchar(50) NOT NULL,
-    email varchar(100) NOT NULL UNIQUE,
-    status varchar(10) DEFAULT 'active'
+      CREATE TABLE IF NOT EXISTS usr (
+        id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        name varchar(50) NOT NULL,
+        email varchar(100) NOT NULL UNIQUE,
+        status varchar(10) DEFAULT 'active'
         CHECK (status IN ('active', 'banned', 'deleted')) NOT NULL
-    );
+      );
     `);
 
     await executeQuery(pool, `
       CREATE TABLE IF NOT EXISTS location (
-    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name varchar(100) NOT NULL,
-    address varchar(200) NOT NULL,
-    price integer NOT NULL,
-    capacity integer NOT NULL,
-    status varchar(10) DEFAULT 'available'
+        id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        name varchar(100) NOT NULL,
+        address varchar(200) NOT NULL,
+        price integer NOT NULL,
+        capacity integer NOT NULL,
+        status varchar(10) DEFAULT 'available'
         CHECK (status IN ('available', 'unavailable')) NOT NULL
-);
+      );
     `);
 
     await executeQuery(pool, `
       CREATE TABLE IF NOT EXISTS meet (
-    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name varchar(50) NOT NULL,
-    status varchar(10) DEFAULT 'active'
-        CHECK (status IN ('active', 'deleted')) NOT NULL,
-    description varchar(200) NOT NULL,
-    is_public boolean NOT NULL,
-    final_place_id bigint REFERENCES location (id),
-    holder_id bigint REFERENCES temp_user (id)
-);
+        id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        name varchar(50) NOT NULL,
+        status varchar(10) DEFAULT 'active'
+            CHECK (status IN ('active', 'deleted')) NOT NULL,
+        description varchar(200) NOT NULL,
+        is_public boolean NOT NULL,
+        final_place_id bigint REFERENCES location (id),
+        holder_id bigint REFERENCES usr (id)
+      );
     `);
 
     await executeQuery(pool, `
       CREATE TABLE IF NOT EXISTS location_option (
-    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    location_id bigint NOT NULL REFERENCES location (id),
-    meet_id bigint NOT NULL REFERENCES meet (id),
-    UNIQUE (location_id, meet_id)
-);
+        id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        location_id bigint NOT NULL REFERENCES location (id),
+        meet_id bigint NOT NULL REFERENCES meet (id),
+        UNIQUE (location_id, meet_id)
+      );
     `);
 
     await executeQuery(pool, `
       CREATE TABLE IF NOT EXISTS participation (
-    meet_id bigint REFERENCES meet (id),
-    user_id bigint REFERENCES temp_user (id),
-    is_pending boolean NOT NULL,
-    PRIMARY KEY (meet_id, user_id)
-);
+        meet_id bigint REFERENCES meet (id),
+        user_id bigint REFERENCES usr (id),
+        is_pending boolean NOT NULL,
+        PRIMARY KEY (meet_id, user_id)
+      );
     `);
 
     await executeQuery(pool, `
       CREATE TABLE IF NOT EXISTS availability (
-    id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    timestamp timestamp NOT NULL,
-    user_id bigint NOT NULL REFERENCES temp_user (id),
-    meet_id bigint NOT NULL REFERENCES meet (id),
-    UNIQUE (timestamp, user_id, meet_id)
-);
+        id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        timestamp timestamp NOT NULL,
+        user_id bigint NOT NULL REFERENCES usr (id),
+        meet_id bigint NOT NULL REFERENCES meet (id),
+        UNIQUE (timestamp, user_id, meet_id)
+      );
     `);
 
     await executeQuery(pool, `
       CREATE TABLE IF NOT EXISTS availability_location (
-    location_option_id bigint NOT NULL,
-    availability_id bigint NOT NULL,
-    PRIMARY KEY (location_option_id, availability_id),
-    FOREIGN KEY (location_option_id) REFERENCES location_option (id)
+      location_option_id bigint NOT NULL,
+      availability_id bigint NOT NULL,
+      PRIMARY KEY (location_option_id, availability_id),
+      FOREIGN KEY (location_option_id) REFERENCES location_option (id)
         ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (availability_id) REFERENCES availability (id)
+      FOREIGN KEY (availability_id) REFERENCES availability (id)
         ON DELETE CASCADE ON UPDATE CASCADE
-);
+      );
     `);
 
     await executeQuery(pool, `
       CREATE TABLE IF NOT EXISTS final_decision (
-          meet_id int REFERENCES meet (id),
-          final_place_id int REFERENCES location (id),
-          final_time timestamp NOT NULL,
-          PRIMARY KEY (meet_id, final_place_id),
+        meet_id int REFERENCES meet (id),
+        final_place_id int REFERENCES location (id),
+        final_time timestamp NOT NULL,
+        PRIMARY KEY (meet_id, final_place_id)
+      );
     `);
 
     console.log('Database schema initialized successfully.');
