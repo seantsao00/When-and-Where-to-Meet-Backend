a229d6dcc6973c1cc9e556360c0917d5f9c2a1dc
Yao <changmingyao2014@gmail.com>
2024-12-10 23:26:34 +0800

    fix(gen): no repeated time&location intervals

--- scripts/Build/build-DB.js
+++
@@ -1,6 +0,0 @@
-import { indexDB, restoreDB } from '../utils.js';
-
-if (import.meta.url === `file://${process.argv[1]}`) {
-  restoreDB(process.env.PGDATABASE);
-  indexDB(process.env.PGDATABASE);
-}
